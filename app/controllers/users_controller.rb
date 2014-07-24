51af6a12934570f36968650275656d4f402a190c
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-07-24 14:40:14 -0700

    Add form for user new and text fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
 	
 	def index
-		# puts "hi"
+		# render homepage
 	end
+
+	def new 
+		# when a user clicks "sign in", verify his account (not sure if this is right place)
+		# when a user clicks "create account", show form
+		@user = User.new 
+	end
+
 
 end
