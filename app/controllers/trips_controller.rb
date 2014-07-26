6005e8075835c461af1d1f6f1f412abeeed83758
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-07-25 18:03:02 -0700

    Fix readability of code

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,24 +1,28 @@
 class TripsController < ApplicationController
+	before_filter :find_user
 	
 	def new
-		@user = User.find(session[:id]) #change to reflect whoever is signed in using sessions
 		@trip = Trip.new 
 	end
 
 	def create
 		@trip = Trip.create(trip_params)
-		# session[:trip_id] = @trip.id
-		@user = User.find(session[:id]) #why do I have to restate this? 
+
 		redirect_to user_trip_path(@user.id, @trip.id) 
 	end
 
 	def show
+		@trip = Trip.find(params[:id])
 	end
 
 	private
 
 	def trip_params
 		params.require(:trip).permit(:where, :when, :price_per_person)
+	end
+
+	def find_user
+		@user = User.find(session[:id])
 	end
 
 end
